9933e0029b70eb1d6d1130a99145cdd05b3def7a
Artem <64410376+Artiss90@users.noreply.github.com>
2021-03-10 15:07:08 +0200

    memorisation getVisibleFilterContacts

--- src/redux/contactsRedux/contactsSelectors.js
+++ src/redux/contactsRedux/contactsSelectors.js
@@ -1,16 +1,19 @@
+import { createSelector } from 'reselect';
+
 const getContactsFilter = state => state.contacts.filter;
 const getContactsItems = state => state.contacts.items;
 const getContactsLoading = state => state.contacts.loading;
 const getContactsError = state => state.contacts.error;
 
-const getVisibleFilterContacts = state => {
-  const normalizedFilter = getContactsFilter(state).toLowerCase();
-  const contacts = getContactsItems(state);
-
-  return contacts.filter(contact =>
-    contact.name.toLowerCase().includes(normalizedFilter),
-  );
-};
+const getVisibleFilterContacts = createSelector(
+  [getContactsFilter, getContactsItems],
+  (filter, allContacts) => {
+    const normalizedFilter = filter.toLowerCase();
+    return allContacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter),
+    );
+  },
+);
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   getContactsFilter,
